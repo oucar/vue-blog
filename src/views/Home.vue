81fc969fb4c543cfc5e33788ac2e6d5f187cda33
oucar <hello@ucaronur.com>
2022-08-21 01:13:26 -0400

    Initial hero section and blogpost preview implementation, small tweaks on navbar and footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <Hero/>
+    <BlogPost/>
   </div>
 </template>
 
 <script>
+import Hero from '@/components/Hero.vue';
+import BlogPost from '@/components/BlogPost.vue';
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Hero,
+    BlogPost,
+    
+  },
+  data() {
+    return {
+      welcomeScreen: {
+        blogPost:
+          "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+    }
+  }
 };
 </script>
