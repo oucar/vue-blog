55225ffc614b3ba60e7fe988f41878108534f13a
oucar <hello@ucaronur.com>
2022-08-24 03:20:59 -0400

    Register and Login page UI changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,23 +5,24 @@
     </vs-button>
     <vs-dialog v-model="active">
       <template #header>
-        <h4 class="not-margin">Welcome to <b>Vuesax</b></h4>
+        <h4 class="not-margin">Welcome to <b>Onur's Blog</b></h4>
       </template>
 
       <div class="con-form">
         <vs-input v-model="email" placeholder="Email">
           <template #icon>
-            @
+            <box-icon name="user"></box-icon>
           </template>
         </vs-input>
         <vs-input type="password" v-model="password" placeholder="Password">
           <template #icon>
-            <i class="bx bxs-lock"></i>
+            <box-icon name="lock-alt"></box-icon>
           </template>
         </vs-input>
         <div class="flex">
           <vs-checkbox v-model="remember">Remember me</vs-checkbox>
-          <a href="#">Forgot Password?</a>
+          <!-- @TODO: Add validation to email, will be only visible if an email is entered -->
+          <a @click="openNotification('bottom-center', 'primary')" class="link">Forgot Password?</a>
         </div>
       </div>
 
@@ -39,15 +40,40 @@
 </template>
 <script>
 export default {
+  name: "Login",
   data: () => ({
     active: false,
     email: "",
     password: "",
     remember: false,
+    forgotPassword: false,
   }),
+  methods: {
+    forgotPasswordClicked() {
+      if (this.forgotPassword === false) this.forgotPassword = true;
+      else {
+        setTimeout(() => {
+          this.forgotPassword = !this.forgotPassword;
+        }, 5000);
+      }
+    },
+    openNotification(position = null, color) {
+      const notification = this.$vs.notification({
+        progress: "auto",
+        color,
+        position,
+        title: "Well, I just sent an e-mail to you.",
+        text: `Please check your email (${this.email}) and click on the provided link to reset your password!`,
+      });
+      console.log(notification);
+    },
+  },
 };
 </script>
 <style lang="scss">
+.link {
+  cursor: pointer;
+}
 .not-margin {
   margin: 0px;
   font-weight: normal;
