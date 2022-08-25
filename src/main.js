b709d5571ebfc43aa132e6d06dea692a4e291a9b
Onur Ucar <onurucar@iu.edu>
2022-08-24 23:32:54 -0400

    Login firebase implementation

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import Vue2Editor from "vue2-editor";
 import Vuesax from "vuesax";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 import '/src/styles/main.scss';
 import 'vuesax/dist/vuesax.css';
@@ -13,10 +15,16 @@ import 'boxicons/css/boxicons.min.css'
 Vue.use(Vuesax);
 Vue.use(Vue2Editor);
 
-Vue.config.productionTip = false;
+let app;
+firebase.auth().onAuthStateChanged(() => {
+  // app is not initialized
+  if (!app) {
+    Vue.config.productionTip = false;
+    new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+  }
+})
