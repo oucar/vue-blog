f917ddbf0444e371dfb89b4bade0f98c86cab93e
Onur Ucar <onurucar@iu.edu>
2022-09-04 20:26:36 -0400

    Blog preview implementation

---
+++ src/views/BlogPreview.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="post-view">
+    <div class="container quillWrapper">
+      <h2>{{ this.blogTitle }}</h2>
+      <img :src="blogCoverPhoto" alt="" />
+      <div class="post-content ql-editor" v-html="blogHTML"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostPreview",
+  computed: {
+    blogTitle() {
+      return this.$store.state.blogTitle;
+    },
+    blogHTML() {
+      return this.$store.state.blogHTML;
+    },
+    blogCoverPhoto() {
+      return this.$store.state.blogPhotoFileURL;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.post-view {
+  min-height: 100%;
+  .container {
+    max-width: 1000px;
+    padding: 60px 25px;
+  }
+  .quillWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+      max-height: 400px;
+      max-width: 100%;
+    }
+  }
+
+  .post-content {
+    img {
+      max-height: 100%;
+      max-width: 100%;
+    }
+  }
+  .ql-editor {
+    padding: 0;
+  }
+  h2 {
+    margin-bottom: 16px;
+    font-weight: 300;
+    font-size: 32px;
+  }
+  img {
+    margin-bottom: 32px;
+  }
+}
+</style>
--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -55,11 +55,12 @@
             :editorOptions="editorSettings"
             v-model="blogHTML"
             useCustomImageHandler
+            @image-added="imageHandler"
           />
         </div>
 
         <div class="blog-actions">
-          <router-link class="router-button" to="#">
+          <router-link class="router-button" :to="{name: 'BlogPreview'}">
             <vs-button class="blog-action-button">Preview</vs-button>
           </router-link>
           <vs-button class="blog-action-button">Publish</vs-button>
@@ -70,6 +71,8 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+import "firebase/storage";
 import Quill from "quill";
 window.Quill = Quill;
 const ImageResize = require("quill-image-resize-module").default;
@@ -123,6 +126,78 @@ export default {
       this.$store.commit("createFileURL", URL.createObjectURL(this.file));
       this.previewButton = false;
     },
+    imageHandler(file, Editor, cursorLocation, resetUploader) {
+      const storageRef = firebase.storage().ref();
+      const docRef = storageRef.child(`documents/blogPostPhotos/${file.name}`);
+      docRef.put(file).on(
+        "state_changed",
+        (snapshot) => {
+          console.log(snapshot);
+        },
+        (err) => {
+          console.log(err);
+        },
+        async () => {
+          const downloadURL = await docRef.getDownloadURL();
+          Editor.insertEmbed(cursorLocation, "image", downloadURL);
+          resetUploader();
+        }
+      );
+    },
+    // uploadBlog() {
+    //   if (this.blogTitle.length !== 0 && this.blogHTML.length !== 0) {
+    //     if (this.file) {
+    //       this.loading = true;
+    //       const storageRef = firebase.storage().ref();
+    //       const docRef = storageRef.child(
+    //         `documents/BlogCoverPhotos/${this.$store.state.blogPhotoName}`
+    //       );
+    //       docRef.put(this.file).on(
+    //         "state_changed",
+    //         (snapshot) => {
+    //           console.log(snapshot);
+    //         },
+    //         (err) => {
+    //           console.log(err);
+    //           this.loading = false;
+    //         },
+    //         async () => {
+    //           const downloadURL = await docRef.getDownloadURL();
+    //           const timestamp = await Date.now();
+    //           const dataBase = await db.collection("blogPosts").doc();
+    //           await dataBase.set({
+    //             blogID: dataBase.id,
+    //             blogHTML: this.blogHTML,
+    //             blogCoverPhoto: downloadURL,
+    //             blogCoverPhotoName: this.blogCoverPhotoName,
+    //             blogTitle: this.blogTitle,
+    //             profileId: this.profileId,
+    //             date: timestamp,
+    //           });
+    //           await this.$store.dispatch("getPost");
+    //           this.loading = false;
+    //           this.$router.push({
+    //             name: "ViewBlog",
+    //             params: { blogid: dataBase.id },
+    //           });
+    //         }
+    //       );
+    //       return;
+    //     }
+    //     this.error = true;
+    //     this.errorMsg = "Please ensure you uploaded a cover photo!";
+    //     setTimeout(() => {
+    //       this.error = false;
+    //     }, 5000);
+    //     return;
+    //   }
+    //   this.error = true;
+    //   this.errorMsg = "Please ensure Blog Title & Blog Post has been filled!";
+    //   setTimeout(() => {
+    //     this.error = false;
+    //   }, 5000);
+    //   return;
+    // },
   },
 };
 </script>
